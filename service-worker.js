2e6b8f0cf9c9b49c5048d82be31ad200240cb34f
catalinacarrasco-beep <catalina.carrasco@grantt.cl>
2026-06-10 15:35:09 -0400

    Add PWA service worker

---
+++ service-worker.js
@@ -0,0 +1,13 @@
+const CACHE = 'ventas-v1';
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['/'])));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).then(res => {
+      const clone = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, clone));
+      return res;
+    }))
+  );
+});
