7bc1f391ab581b205cb0f33b863a0b82e4af4f7c
catalinacarrasco-beep <catalina.carrasco@grantt.cl>
2026-06-18 09:40:15 -0400

    Bump SW cache v4

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,10 @@
-const CACHE = 'ventas-v3';
+const CACHE = 'ventas-v4';
 
-// On install, cache the main page
 self.addEventListener('install', e => {
   self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(['/'])));
 });
 
-// On activate, delete old caches
 self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(keys =>
@@ -15,7 +13,6 @@ self.addEventListener('activate', e => {
   );
 });
 
-// Network first: always try network, fallback to cache
 self.addEventListener('fetch', e => {
   e.respondWith(
     fetch(e.request)
